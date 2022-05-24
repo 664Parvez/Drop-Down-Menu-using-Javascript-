32fa9dbbd7ea63f940fe636b19061180a195f872
Parvez Rabbi <90493258+664Parvez@users.noreply.github.com>
2022-05-24 23:38:25 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let bar = document.getElementById('bar')
+
+bar.addEventListener('click', () => {
+    let main_nav = document.getElementById('main-nav')
+    main_nav.classList.toggle('active')
+})
